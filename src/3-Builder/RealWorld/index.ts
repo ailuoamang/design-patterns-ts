25ac78ea52de4a49e3cfdb3bc1e73aff7579f8df
spacedandy <1152472991@qq.com>
2023-11-24 17:30:43 +0800

    :sparkles:添加注释

--- src/3-Builder/RealWorld/index.ts
+++ src/3-Builder/RealWorld/index.ts
@@ -2,13 +2,6 @@
 构建器设计模式需要的真实例子:有一个具有大量可选参数和一些复杂逻辑的User类
 
 解决方案:创建一个新类，它知道如何按部分构建用户
+some one need email ,maybe not
 */
-enum Gender {Male='Male',Female='Female',Undefined='Undefined'}
-
-export class user {
-  public name: string;
-  public surname: string;
-  public email:string;
-  public gender:Gender;
-
-}
+//这一块书中的例子不好，自己找一个合适点的
