4e069b700c6ce1913653d074161ddbd6b33760d2
埃罗阿芒老师 <1152472991@qq.com>
2023-11-29 21:03:49 +0800

    :sparkles:添加组合模式范例完整

--- src/8-Composite/RealWorld/index.ts
+++ src/8-Composite/RealWorld/index.ts
@@ -50,8 +50,36 @@ class MultiPackageComposite extends PackageComponent {
   }
 }
 
-//客户端代码总是与基本包组件一起工作
-function getGalaxyS68Pack():PackageComponent{
-  const complexMobilePackage=new 
+// Helper (builder)函数隐藏有2个具体的包组件
+function getGalaxyS68Pack(): PackageComponent {
+  const complexMobilePackage = new MultiPackageComposite('Galaxy S68 Pack');
+  complexMobilePackage.add(new ProductLeaf('Galaxy S68', 900));
+  complexMobilePackage.add(new ProductLeaf('S68 Charger', 25));
+  complexMobilePackage.add(new ProductLeaf('S68 Case', 15));
+  return complexMobilePackage;
 }
 
+function getCanonM50Pack(): PackageComponent {
+  const complexCameraPackage = new MultiPackageComposite('Canon M50 Pack');
+  complexCameraPackage.add(new ProductLeaf('Canon M50', 600));
+  complexCameraPackage.add(new ProductLeaf('A50 1.8 Lens', 250));
+  complexCameraPackage.add(new ProductLeaf('128 Gb Micro SD', 40));
+  complexCameraPackage.add(new ProductLeaf('Canon Generic Case', 150));
+  return complexCameraPackage;
+}
+
+function getHeadphones(): PackageComponent {
+  return new ProductLeaf('HyperX Cloud Flight', 150);
+}
+
+//客户端代码总是与基本包组件一起工作
+const galaxyPackage: PackageComponent = getGalaxyS68Pack();
+const canonPackage: PackageComponent = getCanonM50Pack();
+const simpleHeadphonesPackage: PackageComponent = getHeadphones();
+
+const mainShipment: PackageComponent = new MultiPackageComposite('Main Shipment');
+mainShipment.add(galaxyPackage);
+mainShipment.add(canonPackage);
+mainShipment.add(simpleHeadphonesPackage);
+
+console.log(`Total shipment cost: ${mainShipment.getPrice()}€`);
